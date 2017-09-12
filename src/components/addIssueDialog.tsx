67530986382d7218b68d02cf1f26c881849e2798
Atsushi Tanaka <ats124@section-9.tech>
2017-09-12 19:02:47 +0900

    登録課題一覧をDetailsListで表現

--- src/components/addIssueDialog.tsx
+++ src/components/addIssueDialog.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as update from 'immutability-helper'
-import { PrimaryButton, DefaultButton, Dropdown, IDropdownOption } from 'office-ui-fabric-react';
+import { PrimaryButton, DefaultButton, Dropdown, IDropdownOption, DetailsList, IColumn, DetailsListLayoutMode, SelectionMode } from 'office-ui-fabric-react';
 import { BacklogProject, BacklogApiKey } from './backlog-project-selector';
 import * as backlogjs from 'backlog-js';
 
@@ -21,12 +21,14 @@ export interface AddIssueDialogState {
     issues: backlogjs.Option.Issue.PostIssueParams[];
     issueTypeOptions: IDropdownOption[];
     priorityOptions: IDropdownOption[];
+    issueDetailListCoumns: IColumn[];
 }
 
 export class AddIssueDialog extends React.Component<AddIssueDialogProps, AddIssueDialogState> {
     constructor(props, context) {
         super(props, context);
         let {selectedProject, selectedValues} = this.props;
+
         var issues: backlogjs.Option.Issue.PostIssueParams[] = selectedValues.map(row => ({
             projectId: selectedProject.projectId,
             summary: row[0],
@@ -45,8 +47,61 @@ export class AddIssueDialog extends React.Component<AddIssueDialogProps, AddIssu
             text: x.name
         }));
 
+        let issueDetailListCoumns: IColumn[] = [
+            {
+                key: 'column1',
+                name: '種別',
+                fieldName: 'issueTypeId',
+                minWidth: 100,
+                maxWidth: 140,
+                data: 'number',
+                onRender: (item, index) => 
+                    <Dropdown 
+                        options={issueTypeOptions} 
+                        selectedKey={item.issueTypeId} 
+                        onChanged={(item) => 
+                            this.setState({ issues: update(this.state.issues, {[index]: {issueTypeId: {$set: item.key}}}) })
+                        }
+                    />
+            },
+            {
+                key: 'column2',
+                name: '優先度',
+                fieldName: 'priorityId',
+                minWidth: 100,
+                maxWidth: 140,
+                data: 'number',
+                onRender: (item, index) => 
+                    <Dropdown 
+                        options={priorityOptions} 
+                        selectedKey={item.priorityId} 
+                        onChanged={(item) => 
+                            this.setState({ issues: update(this.state.issues, {[index]: {priorityId: {$set: item.key}}}) })
+                        }
+                    />
+            },                    
+            {
+                key: 'column3',
+                name: '件名',
+                fieldName: 'summary',
+                minWidth: 200,
+                maxWidth: 300,
+                data: 'string',
+                onRender: (item) => <span data-is-focusable={ true }>{ item.summary }</span>
+            },
+            {
+                key: 'column4',
+                name: '詳細',
+                fieldName: 'description',
+                minWidth: 300,
+                maxWidth: 400,
+                data: 'string',
+                onRender: (item) => <span data-is-focusable={ true }>{ item.description }</span>
+            }
+        ];
+
         this.state = {
-            issues, issueTypeOptions, priorityOptions
+            issues, issueTypeOptions, priorityOptions, issueDetailListCoumns
         };
     }
 
@@ -54,41 +109,16 @@ export class AddIssueDialog extends React.Component<AddIssueDialogProps, AddIssu
     }
         
     render() {
-        let { issues, issueTypeOptions, priorityOptions } = this.state;
+        let { issues, issueDetailListCoumns } = this.state;
         return (
             <div>
-                <table className='ms-Table'>
-                    <thead>
-                        <tr>
-                            <th>種別</th>
-                            <th>件名</th>
-                            <th>詳細</th>
-                            <th>優先度</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {issues.map((issue, index) => 
-                        <tr>
-                            <td>
-                                <Dropdown 
-                                    options={issueTypeOptions} 
-                                    selectedKey={issue.issueTypeId} 
-                                    onChanged={(item) => {
-                                        console.log(item.key);
-                                        this.setState({ issues: update(this.state.issues, {[index]: {issueTypeId: {$set: item.key}}}) })
-                                    }}/>
-                            </td>
-                            <td>{issue.summary}</td>
-                            <td>{issue.description}</td>
-                            <td>
-                                <Dropdown 
-                                    options={priorityOptions} 
-                                    selectedKey={issue.priorityId} 
-                                    onChanged={(item) => this.setState({ issues: update(this.state.issues, {[index]: {priorityId: {$set: item.key}}}) })}/>
-                            </td>
-                        </tr>)}
-                    </tbody>
-                </table>
+                <DetailsList
+                    items={ issues }
+                    columns={ issueDetailListCoumns }
+                    layoutMode={ DetailsListLayoutMode.justified }
+                    isHeaderVisible={ true }
+                    selectionMode={ SelectionMode.none }
+                />
                 <footer className='ms-u-textAlignRight'>
                     <PrimaryButton>登録</PrimaryButton>
                     <DefaultButton>キャンセル</DefaultButton>
